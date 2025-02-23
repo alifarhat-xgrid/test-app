67a6704e83fc38d2ce0178d4bd3631d8a0f1b01b
alifarhat-xgrid <ali.farhat@xgrid.co>
2025-02-23 19:41:14 +0500

    Server v2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World! Deployment successful 🎉");
+  res.send("Hello, World! Deployment v2 successful 🎉");
 });
 
 app.listen(PORT, () => {
